fa4d6076dba38a66997b3e7a5023c254899d2031
raymond Emetulu <emetuluray@gmail.com>
2022-12-08 09:14:14 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ *
+ * Description: A program that prints with put function
+ *
+ *Return: Always 0
+ */
+int main (void)
+{
+	puts("\" Programming is like building a multilingual puzzle\n");
+	return (0);
+}
