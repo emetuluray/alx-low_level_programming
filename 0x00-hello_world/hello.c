0a3f40875a1e9ca932f2798cb944241904c4872d
raymond Emetulu <emetuluray@gmail.com>
2022-12-08 07:55:16 -0800

    preprocessor

---
+++ 0x00-hello_world/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-  this is prints hello raymoney first c program in alx
+ * return- returns 0  i want it to count
+ */
+
+int main(void) /*betty is crazy*/
+{
+	printf("Hello raymoney");/*print */
+	return (0);
+}
