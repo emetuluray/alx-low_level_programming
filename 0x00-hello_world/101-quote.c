2e1537e31d7323deca388f6e479f1804f4470c5a
raymond Emetulu <emetuluray@gmail.com>
2022-12-08 18:13:34 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- Entry
+ * Return: Always return 1
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora korpar, 2015-10-19\n")
+	return (1);
+}
