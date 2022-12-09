87f28d628f2f3cb0a1e1f73cd352f3b1509fc3fb
raymond Emetulu <emetuluray@gmail.com>
2022-12-09 15:32:51 -0800

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,13 @@
  * return: Always 0
  */
 
-int main(void)
+int main(void) /*program to print alphabet except e an q*/
 {
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if(ch != 'e' && ch != 'q')
+		if (ch != 'e' && ch != 'q')
 			putchar (ch);
 	}
 	putchar('\n');
