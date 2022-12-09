3e59d24cdcf9a4abca707799c36b89414c3daf3b
raymond Emetulu <emetuluray@gmail.com>
2022-12-08 16:46:08 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * printf: it prints with proer grammer
  * return: returns 0
  */
-int main(void);
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
