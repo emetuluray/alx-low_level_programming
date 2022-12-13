a648c1b4032c9d69a9c839fb954e9d3092348940
raymond Emetulu <emetuluray@gmail.com>
2022-12-12 20:46:38 -0800

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* main - Entry
+ * Return: Always 0
+ */
+
+int main()
+{
+	int a, b;
+
+	for (a = '0'; a, '9'; a++)
+	{
+		for (b =a +1; b <= '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
