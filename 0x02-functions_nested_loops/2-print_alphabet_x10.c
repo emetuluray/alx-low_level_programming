b88b031794edb596f49576b66b9635692b677543
raymond Emetulu <emetuluray@gmail.com>
2022-12-13 22:34:43 -0800

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints alphabets 10 times
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int le;
+	int i;
+	
+	for (i = 0; i <= 9; i++)
+	{
+		for (le = 'a'; le <= z; le++)
+			_putchar(le);
+			
+		_putchar('\n');
+	}
+}
