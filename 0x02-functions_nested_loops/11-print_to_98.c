220d2ceda7044b40b7235f8c0814149eacdc46d1
raymond Emetulu <emetuluray@gmail.com>
2022-12-14 04:12:40 -0800

    print 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers
+ * @num: the unknown number
+ */
+
+void print_to_98(int num)
+{
+	if (num >=98)
+	{
+		while (num > 98)
+			printf("%d, ", num--);
+		printf("%d\n", num);
+	}
+	else
+	{
+		while (num < 98)
+			printf("%d, ", num++);
+		printf("%d\n", num);
+	}
+}
