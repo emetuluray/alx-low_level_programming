b607473fc5796b96e1f6130d9207bb5f43b19f35
raymond Emetulu <emetuluray@gmail.com>
2022-12-15 16:07:36 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H_
+#define _MAIN_
+
+int _putchar(char c);
+imt _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int sie);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
