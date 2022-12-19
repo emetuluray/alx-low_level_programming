74493502561d7b986192628f72dc08ea7ab6abfc
raymond Emetulu <emetuluray@gmail.com>
2022-12-19 06:22:31 -0800

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns value of length of a string
+ * @s: character to check
+ * Description: this will return length of string
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return(a);
+}
