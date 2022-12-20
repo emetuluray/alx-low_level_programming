aa24dc54e694a60408994b76118ca5c382e6a227
raymond Emetulu <emetuluray@gmail.com>
2022-12-19 16:57:20 -0800

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,14 @@ void puts_half(char *str)
 {
 	int index = 0, len = 0, n;
 
-	while(str[index++])
+	while (str[index++])
 		len++;
 
 	if ((len % 2) == 0)
 		n = len / 2;
 
 	else
-		n = (len + 1) /2;
+		n = (len + 1) / 2;
 
 	for (index = n; index < len; index++)
 		_putchar(str[index]);
