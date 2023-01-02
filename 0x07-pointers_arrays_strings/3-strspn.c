71206c8c6faf8d666f123f04f37b5931ab37662d
raymond Emetulu <emetuluray@gmail.com>
2023-01-02 05:58:00 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - a function tht gets the length of a substring
+ * @s: the initial segment
+ * @accept: which consist of bytes only
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+	return (z);
+}
