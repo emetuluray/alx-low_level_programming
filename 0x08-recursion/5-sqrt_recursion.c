3d2911aafef493404f070a36d14013c7ad90bd84
raymond Emetulu <emetuluray@gmail.com>
2023-01-04 13:13:10 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int check(int a, int b)
 		return (a);
 	if (a * a > b)
 		return (-1);
-	return (check (a + 1, b))
+	return (check(a + 1, b));
 }
 /**
  * _sqrt_recursion - returns the naturl square root of a number
