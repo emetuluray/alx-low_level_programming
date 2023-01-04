4046d562dc40b09a82863abd51b1797f2f995691
raymond Emetulu <emetuluray@gmail.com>
2023-01-04 11:43:56 -0800

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * _puts_recursion - print a string
- * @s: sring 
+ * @s: string
  * Return: void
  */
 void _puts_recursion(char *s)
 {
 	if (*s)
-
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
