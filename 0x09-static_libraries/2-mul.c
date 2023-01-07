64fb44f2cd571856d8714ee30aa2637789f98ab6
raymond Emetulu <emetuluray@gmail.com>
2023-01-06 20:14:35 -0800

    library

---
+++ 0x09-static_libraries/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integer
+ * @a: the first integer
+ * @b: the second integer
+ * Return: the result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x09-static_libraries/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line -Draws a straight line in the terminal
+ * using
+ * @n: the number of character to be printed
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('_');
+	}
+
+	_putchar('\n');
+}
