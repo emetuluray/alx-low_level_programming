3ba79311681cb66af8a7cb3444452f7758078e5c
raymond Emetulu <emetuluray@gmail.com>
2023-01-09 04:27:22 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,7 +7,7 @@
  * main - adds positive numbers
  * @argc: n args
  * @argv: arr args
- * Return: 0
+ * Return: Always 0;
  */
 int main(int argc, char *argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	if(argc < 3)
+	if (argc < 3)
 	{
 		printf("%d\n", 0);
 		return (0);
