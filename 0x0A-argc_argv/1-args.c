f93f85652d8b919118158ded1bc65933de2ae609
raymond Emetulu <emetuluray@gmail.com>
2023-01-09 03:29:31 -0800

    argument counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints number of argument
+ * @argc: first parameter
+ * @argv: second parameter
+ *
+ * Description: prints number of argument
+ * Return: Always 0;
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n\n", argc);
+	return (0);
+}
